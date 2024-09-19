e1f7d616784a42421a6e7df5a315927a18ddbeba
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-09-19 13:37:52 -0400

    desktop version is ready

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(function () {
+  $(".design__slider").slick({
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow:
+      '<img src="/images/arrow-left.svg" alt="" class="arrow arrow-left">',
+    nextArrow:
+      '<img src="/images/arrow-right.svg" alt="" class="arrow arrow-right" />',
+  });
 });
